dc4c7a5fb6a098bd797ca0eb9bba0c7bc933d103
urinaidoo <111341234+urinaidoo@users.noreply.github.com>
2023-03-07 15:16:28 +0200

    3.2 Main Function to Display Age
    Instance of struct created with my age and birth date. 
    Function to display counter until my age on LCD screen added.

--- main.c
+++ main.c
@@ -27,10 +27,25 @@ struct age_data my_age;
 
 void main(void)
 {
+	init_LCD();
+	my_age.day = 17;
+	my_age.month = 11;
+	my_age.year = 2001;
+	my_age.age = 21;
+	char str[2];
 
 
 	while(1)
 	{
+		int i;
+		int age_int = my_age.age;
+		for(i = 0;i<=age_int;++i){
+		sprintf(str,"%d",i);
+		lcd_putstring(str);
+		delay(65000);
+		lcd_command(CLEAR);
+		delay(65000);
+
 
 	}
 }
