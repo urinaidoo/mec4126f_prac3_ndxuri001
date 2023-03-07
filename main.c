4fbf3a4095d4981edd1f632d7076be95f759a4e2
urinaidoo <111341234+urinaidoo@users.noreply.github.com>
2023-03-07 15:11:55 +0200

    3.1 age_data Struct
    Adding libraries and constructing the struct.

--- main.c
+++ main.c
@@ -7,13 +7,21 @@
 #define STM32F051												   //COMPULSORY
 #include "stm32f0xx.h"											   //COMPULSORY
 #include "lcd_stm32f0.h"
+#include <stdio.h>
+#include <string.h>
 
 // GLOBAL VARIABLES ----------------------------------------------------------|
+struct age_data {
+	uint8_t day;
+	uint8_t month;
+	uint16_t year;
+	uint8_t age;
+};
 
+struct age_data my_age;
 
 // FUNCTION DECLARATIONS -----------------------------------------------------|
 
-void main(void);                                                   //COMPULSORY
 
 // MAIN FUNCTION -------------------------------------------------------------|
 
